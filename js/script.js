f78ad82fb946ca1708f5ffe767436360b35cc352
Matheus <matheus.physicist369@gmail.com>
2024-10-03 16:22:30 -0300

    Att-button

--- js/script.js
+++ js/script.js
@@ -5,3 +5,23 @@ function ModalSuccess(){
         icon: "success"
       });
 }
+function Cart(){
+
+    Swal.fire({
+        title: "Do you want to buy this?",
+        text: "Click here to finish",
+        icon: "warning",
+        showCancelButton: true,
+        confirmButtonColor: "#3085d6",
+        cancelButtonColor: "#d33",
+        confirmButtonText: "Yes!"
+      }).then((result) => {
+        if (result.isConfirmed) {
+          Swal.fire({
+            title: "Bought",
+            text: "Bought successfully.",
+            icon: "success"
+          });
+        }
+      });
+}
